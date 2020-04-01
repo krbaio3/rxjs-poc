528c91511034185d13d59c86f7c58281084e64e1
J. KrBaIO3 <krbaio3@gmail.com>
2020-04-01 13:42:36 +0200

    observables

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
+import { Observable, Observer } from 'rxjs';
+
+const observer: Observer<any> = {
+    next: value => console.log('siguiente [next]:', value),
+    error: err => console.warn('error obs: ', err),
+    complete: () => console.info('completado'),
+};
+
+const obs$ = new Observable( subscriber => {
+    subscriber.next('Hola');
+    subscriber.next('Mundo');
+
+    // Forzar error
+    // const a = undefined;
+    // a.nombre = 'Tomas';
+
+    // subscriber.error('muerto');
+
+    subscriber.complete();
+
+    subscriber.next('Muerte cerdo')
+});
 
 
-console.log('Hola Mundo!');
+// obs$.subscribe(
+//     valor => console.log('next: ', valor),
+//     error => console.error('error: ', error),
+//     () => console.log('completado'),
+// );
 
-
-
-
-
-
-
+obs$.subscribe(observer);
