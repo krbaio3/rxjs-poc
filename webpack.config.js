0d6625c725be2f09eff4fc0aa904e8474bf2999c
J. KrBaIO3 <krbaio3@gmail.com>
2020-04-07 15:49:00 +0200

    new webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,55 @@
 const {resolve} = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+    entry: './src/index.ts',
     output: {
         filename: 'bundle.js',
-        path: resolve(__dirname, 'dist')
+        path: resolve(__dirname, 'dist'),
     },
-    entry: './src/index.ts',
     devtool: 'inline-source-map',
     module: {
         rules: [
             {
-                test: /\.tsx?$/,
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.ts(x)?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.png$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            mimetype: 'image/png'
+                        }
+                    }
+                ]
             }
         ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+            inject: true,
+            filename: 'index.html',
+            title: 'RxJS',
+        }),
+        new MiniCssExtractPlugin()
+    ],
     resolve: {
         extensions: [ '.tsx', '.ts', '.js' ]
     },
